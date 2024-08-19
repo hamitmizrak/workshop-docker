7d0def85f95986547eaeb67c801d5b4a3a9d70bd
Hamit Mizrak <hamitmizrak@outlook.com>
2024-08-19 09:23:10 +0300

    bashscript

---
+++ project.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Docker Info
+docker --version
+docker version
+docker -v
+docker info
+
+#--------------------------------
+docker run hello-world
+docker container run hello-world
+
+#--------------------------------
+docker search nginx
+docker pull nginx
+docker pull nginx
+docker pull mysql
+docker pull httpd
+docker pull alpine
+docker pull ubuntu
+docker pull centos
